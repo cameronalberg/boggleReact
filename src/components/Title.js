3ce6b622fddf3cc9da949f365efe0fbe680abf4d
Cameron Alberg <cameronalberg@gmail.com>
2022-09-21 20:36:47 -0500

    replaced favicon, deleted old logo, updated title info

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,7 +6,7 @@ const Title = () => {
         <div className="card">
             <p className="title">Boggle</p>
             <p className="descriptor">Built with React by Cameron Alberg </p>
-            <p className="link"><a href="https://github.com/cameronalberg/boggleReact">(source code)</a></p>
+            <p className="link"><a href="https://github.com/cameronalberg/boggleReact">(source code)</a> <a href="https://boggle-api.calberg.me">(API usage)</a></p>
         </div>
     )
 
