1a066eb7b58ccfb9408f540ff292116600de2091
Cameron Alberg <cameronalberg@gmail.com>
2022-10-11 23:18:43 -0500

    updated to use bootstrap tooltips

--- src/components/results/words/WordEntry.js
+++ src/components/results/words/WordEntry.js
@@ -1,28 +1,54 @@
 import './WordEntry.css'
 import {useState} from "react";
-import ReactTooltip from "react-tooltip";
-import Definition from "./Definition";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 const WordEntry = (props) => {
-    const [displayDef, setDisplayDef] = useState(false)
+    const [definition, setDefinition] = useState("")
+
+    const getDefinition = () => {
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${props.word.word}`)
+            .then((response) => {
+                if(response.status !== 200){
+                    setDefinition("not found.")
+                } else {
+                    return response.json()
+                }
+            })
+            .then((data) => parseDefinition(data))
+            .then((result) => setDefinition(result))
+        return true
+    }
+
+    const parseDefinition = (data) => {
+        if (data !== null) {
+            try {
+                const meanings = data[0].meanings
+                let def = (meanings[0].definitions)[0].definition
+                def = def.toString()
+                return def
+            } catch {
+                return "not found."
+            }
+        }
+        return "not found."
+    }
+    const renderTooltip = props => (
+        <Tooltip className="tooltip" {...props}>{definition}</Tooltip>
+    );
     return (
         <div>
-            <div className="word" data-tip="definition" data-for="definition"
-                 onMouseEnter={() => {
-                     props.path(props.word.path);
-                     setDisplayDef(true);
-                    }}
-                 onMouseLeave={() => {
-                     props.path(false);
-                     setDisplayDef(false);
-                 }}>
-                {props.word.word}
-            </div>
-            {displayDef && (
-                <ReactTooltip id="definition" type="error" event="click" eventOff="hover">
-                    <Definition word={props.word.word} />
-                </ReactTooltip>
-            )}
+            <OverlayTrigger  onEnter={getDefinition} placement="top" overlay={renderTooltip}>
+                <div className="word" data-tip="definition" data-for="definition"
+                     onMouseEnter={() => {
+                         props.path(props.word.path);
+                        }}
+                     onMouseLeave={() => {
+                         props.path(false);
+                     }}>
+                    {props.word.word}
+                </div>
+            </OverlayTrigger>
         </div>
     )
 }
